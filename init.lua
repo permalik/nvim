932ff210eb831d8fccb9bb84a35f81bc770257d5
permalik <permalik@protonmail.com>
2024-03-16 20:42:53 -0500

    colorscheme and remaps

--- init.lua
+++ init.lua
@@ -16,13 +16,17 @@
       - Tutor
       - <enter key>
 --]]
+local opts = { silent = true }
 
 -- <space> is leader
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
 -- netrw file browser
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+-- colorscheme
+vim.keymap.set('n', '<leader>kl', ':colorscheme kanagawa-lotus<CR>', opts)
+vim.keymap.set('n', '<leader>kw', ':colorscheme kanagawa-wave<CR>', opts)
+vim.keymap.set('n', '<leader>kd', ':colorscheme kanagawa-dragon<CR>', opts)
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = true
@@ -748,13 +752,12 @@ require('lazy').setup({
         -- change the command in the config to whatever the name of that colorscheme is
         --
         -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-        'folke/tokyonight.nvim',
+        'rebelot/kanagawa.nvim',
         priority = 1000, -- make sure to load this before all the other start plugins
         init = function()
-            -- Load the colorscheme here.
-            -- Like many other themes, this one has different styles, and you could load
-            -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-            vim.cmd.colorscheme 'tokyonight-night'
+            vim.cmd.colorscheme 'kanagawa-lotus'
+            vim.cmd.colorscheme 'kanagawa-wave'
+            vim.cmd.colorscheme 'kanagawa-dragon'
 
             -- You can configure highlights by doing something like
             vim.cmd.hi 'Comment gui=none'
