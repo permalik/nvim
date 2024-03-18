cbe3b225c4eadcb1dfb9644dce2a44ab2e88ebf5
permalik <permalik@protonmail.com>
2024-03-18 09:16:09 -0500

    update harpoon bindings; add lss

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -75,9 +75,6 @@ return {
             require('cmp_nvim_lsp').default_capabilities()
         )
 
-        -- Enable the following language servers
-        --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-        --
         --  Add any additional override configuration in the following tables. Available keys are:
         --  - cmd (table): Override the default command used to start the server
         --  - filetypes (table): Override the default list of associated filetypes for the server
@@ -85,18 +82,30 @@ return {
         --  - settings (table): Override the default settings passed when initializing the server.
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-            -- clangd = {},
-            -- gopls = {},
+            -- astro = {},
+            clangd = {},
+            -- csharp_ls = {},
+            cssls = {},
+            -- elixirls = {},
+            emmet_language_server = {},
+            -- htmx = {},
+            gopls = {},
+            jsonls = {},
+            marksman = {},
+            -- ocamlls = {},
+            -- ocamllsp = {},
+            -- omnisharp = {},
+            -- postgres_lsp = {},
             -- pyright = {},
-            -- rust_analyzer = {},
-            -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-            --
-            -- Some languages (like typescript) have entire language plugins that can be useful:
-            --    https://github.com/pmizio/typescript-tools.nvim
-            --
-            -- But for many setups, the LSP (`tsserver`) will work just fine
-            -- tsserver = {},
-            --
+            rust_analyzer = {},
+            -- sqlls = {},
+            -- sqls = {},
+            -- svelte = {},
+            -- tailwindcss = {},
+            -- templ = {},
+            tsserver = {},
+            volar = {},
+            vuels = {},
 
             lua_ls = {
                 -- cmd = {...},
