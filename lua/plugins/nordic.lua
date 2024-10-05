95f57606318c0c7a0cdf743840379385260833ea
permalik <permalik@protonmail.com>
2024-10-05 17:52:44 -0500

    update nordic visual and visualline

--- lua/plugins/nordic.lua
+++ lua/plugins/nordic.lua
@@ -3,8 +3,24 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+        require("nordic").setup({
+            on_highlight = function(highlights, palette)
+                highlights.Visual = { bg = palette.gray3 }
+                highlights.VisualLine = { bg = palette.gray3 }
+                -- highlights.Normal = { fg = palette.red.bright, bg = palette.green.base }
+                -- highlights.Comment = { fg = palette.red.bright, italic = true }
+                -- highlights.Function = { fg = palette.red.bright, bold = true }
+                -- highlights.TelescopePromptTitle = {
+                --     fg = palette.red.bright,
+                --     bg = palette.green.base,
+                --     italic = true,
+                --     underline = true,
+                --     sp = palette.yellow.dim,
+                --     undercurl = false
+                -- }
+            end,
+        })
         require("nordic").load()
-
         vim.cmd 'colorscheme nordic'
     end
 }
