c6dcf5a32a7fec79797408e1ee38aad2e7956b81
permalik <permalik@protonmail.com>
2024-11-18 19:23:05 -0600

    feat: add null_ls, prettier

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,6 +19,7 @@ return {
             "WhoIsSethDaniel/mason-tool-installer.nvim",
             { "j-hui/fidget.nvim",       opts = {} },
             "hrsh7th/cmp-nvim-lsp",
+            "MunifTanjim/prettier.nvim",
         },
         config = function()
             vim.api.nvim_create_autocmd("LspAttach", {
@@ -252,6 +253,47 @@ return {
                 },
             }
         end,
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+
+            null_ls.setup({
+                on_attach = function(client, bufnr)
+                    if client.resolved_capabilities.document_formatting then
+                        vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+
+                        -- format on save
+                        vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
+                    end
+
+                    if client.resolved_capabilities.document_range_formatting then
+                        vim.cmd("xnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.range_formatting({})<CR>")
+                    end
+                end,
+            })
+
+            local prettier = require("prettier")
+
+            prettier.setup({
+                bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
+                filetypes = {
+                    "css",
+                    "graphql",
+                    "html",
+                    "javascript",
+                    "javascriptreact",
+                    "json",
+                    "less",
+                    "markdown",
+                    "scss",
+                    "typescript",
+                    "typescriptreact",
+                    "yaml",
+                },
+            })
+        end
     },
     {
         "stevearc/conform.nvim",
